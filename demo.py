f9b7d267da99d9e51f060677488f8d032fb87219
CUGer3888 <152231619+CUGer3888@users.noreply.github.com>
2024-09-01 15:18:58 +0800

    Add files via upload

--- demo.py
+++ demo.py
@@ -10,7 +10,8 @@ import math
 from torch.nn import functional as F
 import torchvision.transforms as transforms
 detector = MTCNN()
-model = FaceNetmModel()
+model = FaceNetmModel(256, 108)
+
 
 
 def cv_to_pil(img):
@@ -37,46 +38,53 @@ def face_cor(img):
     angle = math.degrees(math.atan(k))
     img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     img = img.rotate(angle, expand=True)
-    print(bbox)
-    face_img = img.crop((bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]))
+    face_img = img.crop((bbox[0], bbox[1],bbox[0]+ bbox[2],bbox[1]+ bbox[3]))
     return pil_to_cv(face_img)
-
-
 def face_net(img1, img2):
     pred_arr1, mpead_arr2 = model(img1), model(img2)
     return F.pairwise_distance(pred_arr1, mpead_arr2)
 
+def pridict(path1,path2):
 
-def pridict(path):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = FaceNetmModel(256, 108)
-    model.load_state_dict(torch.load('facenet.pth'))
-    model = model.to(device)
-
-    model.eval()  # 预测模式
-
-    # 获取测试图片，并行相应的处理
-    img = cv2.imread(path)
-    img = face_cor(img)
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
+    model.to(device)
+    model.load_state_dict(torch.load('facenet.pth', map_location=device))
+    model.eval()
     transform = transforms.Compose([transforms.Resize(256),  # 重置图像分辨率
                                     transforms.CenterCrop(128),  # 中心裁剪
                                     transforms.ToTensor(), ])
+
+    img = cv2.imread(path1)
+    img = face_cor(img)
     img = Image.fromarray(img)
-    img = img.convert("RGB")  # 如果是标准的RGB格式，则可以不加
     img = transform(img)
     img = img.unsqueeze(0)
     img = img.to(device)
+    # print("test")
+    img1 = cv2.imread(path2)
+    img1 = face_cor(img1)
+    img1 = Image.fromarray(img1)
+    img1 = transform(img1)
+    img1 = img1.unsqueeze(0)
+    img1 = img1.to(device)
+    #
+    distance = face_net(img, img1)
+    print("预测结果", distance)
+    # with torch.no_grad():
+    #     py = model(img.to(device))
+    #     print(py)
+    #     #求平均值
+    #     py = py.mean(dim=1)
+    #     print(py)
 
-    with torch.no_grad():
-        py = model(img)
-    predicted, _ = torch.max(py, 1)  # 获取分类结果
-    classIndex_ = predicted[0]
-
-    print('预测结果', classIndex_)
 # 预测结果 tensor(8.5014, device='cuda:0')
 # 预测结果 tensor(8.1327, device='cuda:0')
-# 效果还不错
+
+#效果很好
+
 if __name__ == '__main__':
-    pridict('test1.jpg')
+    while True:
+        path1 = input("请输入图片1路径：")
+        path2 = input("请输入图片2路径：")
+        pridict(path1, path2)
+
